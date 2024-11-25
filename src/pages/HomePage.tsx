95bdac96f706d439d4cc4e80296d78090f2e35d2
Mathieu Keca <mathieu.keca@gmail.com>
2024-11-25 11:41:42 +0100

    fix picture in picture

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -80,6 +80,7 @@ export default function HomePage() {
           autoPlay
           loop
           muted
+          disablePictureInPicture
         />
         <div className="absolute inset-0 bg-black bg-opacity-60 flex items-center justify-center">
           <div className="text-center">
